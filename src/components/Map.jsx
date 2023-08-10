828c1eecae7a7f2737f1b80da918eeb2db3da279
gateremark <mark5gatere@gmail.com>
2023-08-10 14:18:00 +0300

    Initial commit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,11 +2,10 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useMemo } from "react";
 
 const Map = () => {
-
-    const center = useMemo(
-			() => ({ lat: -0.3921935964842486, lng: 36.95869511962907 }),
-			[],
-		);
+	const center = useMemo(
+		() => ({ lat: -0.3921935964842486, lng: 36.95869511962907 }),
+		[],
+	);
 	const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
 	const { isLoaded } = useLoadScript({
@@ -14,7 +13,6 @@ const Map = () => {
 	});
 
 	if (!isLoaded) return <div>Loading...</div>;
-    
 
 	return (
 		<GoogleMap
@@ -22,7 +20,7 @@ const Map = () => {
 			center={center}
 			mapContainerClassName=" w-[100%] h-[100vh]"
 		>
-			<Marker position={center} />
+			<Marker position={{ lat: -0.3921935964842486, lng: 36.95869511962907 }} />
 		</GoogleMap>
 	);
 };
