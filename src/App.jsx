6fb7cd1a81cc1e9df6f0e8621f5b01f69773035e
gateremark <mark5gatere@gmail.com>
2023-08-10 14:14:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Map from "./components/Map";
+
+function App() {
+
+	
+	return (
+		<Map />
+	);
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,30 @@
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useMemo } from "react";
+
+const Map = () => {
+
+    const center = useMemo(
+			() => ({ lat: -0.3921935964842486, lng: 36.95869511962907 }),
+			[],
+		);
+	const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: apiKey,
+	});
+
+	if (!isLoaded) return <div>Loading...</div>;
+    
+
+	return (
+		<GoogleMap
+			zoom={10}
+			center={center}
+			mapContainerClassName=" w-[100%] h-[100vh]"
+		>
+			<Marker position={center} />
+		</GoogleMap>
+	);
+};
+
+export default Map;
